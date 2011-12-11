df008ddd2b38377fff4ee1ee0cec8803b0913bb8
Derek Woods <derekrw@gmail.com>
2011-12-10 22:38:38 -0500

    Example and readme

--- Example.java
+++ Example.java
@@ -12,46 +12,46 @@ public FlowMenu menu;
 public void init() {
 		menu = new FlowMenu(options.camera, FlowMenu.FLOAT, FlowMenu.CENTER);
 
-	    // Preloaded texture
-        final SpriteMenuItem nextMenuItem = new SpriteMenuItem(Options.MENU_NEXT, textureNext);
-        nextMenuItem.setBlendFunction(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
-   	
-   		// Transparent background over our game for the menu
-    	Rectangle rect = new Rectangle(0,0, CAMERA_WIDTH, CAMERA_HEIGHT);
-    	rect.setColor(0.0f, 0.0f, 0.0f);
-    	rect.setAlpha(0.8f);
-    	
-    	BitmapTextureAtlas fontTexture = new BitmapTextureAtlas(256, 256, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
-        Font font = new Font(fontTexture, Typeface.create(Typeface.DEFAULT, Typeface.BOLD), 32, true, Color.WHITE);
-        getEngine().getTextureManager().loadTexture(fontTexture);
-        getEngine().getFontManager().loadFont(font);
+		// Preloaded texture
+		final SpriteMenuItem nextMenuItem = new SpriteMenuItem(Options.MENU_NEXT, textureNext);
+		nextMenuItem.setBlendFunction(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
 
-        // Create our menu
-        menu.init()
-	        .add(rect)
-	        .br(90, true)
-	        .container(FlowMenu.CENTER)
-	        	.container(FlowMenu.CENTER)
-	        		.add(options.statusText)
-	        	.end()
+			// Transparent background over our game for the menu
+		Rectangle rect = new Rectangle(0,0, CAMERA_WIDTH, CAMERA_HEIGHT);
+		rect.setColor(0.0f, 0.0f, 0.0f);
+		rect.setAlpha(0.8f);
+
+		BitmapTextureAtlas fontTexture = new BitmapTextureAtlas(256, 256, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+		Font font = new Font(fontTexture, Typeface.create(Typeface.DEFAULT, Typeface.BOLD), 32, true, Color.WHITE);
+		getEngine().getTextureManager().loadTexture(fontTexture);
+		getEngine().getFontManager().loadFont(font);
+
+		// Create our menu
+		menu.init()
+			.add(rect)
+			.br(90, true)
+			.container(FlowMenu.CENTER)
+				.container(FlowMenu.CENTER)
+					.add(options.statusText)
+				.end()
 				.br(40)
-	    		.container()
-	    			.add(new Text(0, 0, font, "Score:"),250)
-	    			.add(options.scoreText)
-	    			.br()
-	    			.add(new Text(0, 0, font, "Splits:"),250)
-	    			.add(options.splitsText)
-	    		.end()
-	        	.br(40)
-	        	.container(FlowMenu.CENTER)
-	        		.add(nextMenuItem)
-	        	.end()
-        	.end();
-        
-        menu.setMenuAnimator(new FlowAnimator(0.5f));
-        menu.buildAnimations();
-        menu.setBackgroundEnabled(false);
-        menu.setOnMenuItemClickListener(this);
+				.container()
+					.add(new Text(0, 0, font, "Score:"),250)
+					.add(options.scoreText)
+					.br()
+					.add(new Text(0, 0, font, "Splits:"),250)
+					.add(options.splitsText)
+				.end()
+				.br(40)
+				.container(FlowMenu.CENTER)
+					.add(nextMenuItem)
+				.end()
+			.end();
+
+		menu.setMenuAnimator(new FlowAnimator(0.5f));
+		menu.buildAnimations();
+		menu.setBackgroundEnabled(false);
+		menu.setOnMenuItemClickListener(this);
 }
 
 // Show the menu
